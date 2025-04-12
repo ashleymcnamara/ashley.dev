b61467c294e391639b4d58bfae100d8635ddc919
Ashley Willis <ashleymcnamara1@gmail.com>
2025-04-12 01:39:18 +0000

    Improve tabs layout for mobile portrait mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ export default {
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
     darkMode: 'class',
     theme: {
+        screens: {
+            'xs': '480px',
+            'sm': '640px',
+            'md': '768px',
+            'lg': '1024px',
+            'xl': '1280px',
+            '2xl': '1536px',
+        },
         extend: {
             colors: {
                 primary: '#F43F5E'
