e58f507a2880dd1edf145d2648c2f0c61213e05a
Sean Killeen <SeanKilleen@gmail.com>
2025-04-22 18:58:18 +0000

    Update to use posts collection

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,4 +1,7 @@
 import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+
+const posts = await getCollection('posts');
 
 export function GET(context) {
   return rss({
@@ -11,7 +14,14 @@ export function GET(context) {
     site: context.site,
     // Array of `<item>`s in output xml
     // See "Generating items" section for examples using content collections and glob imports
-    items: [],
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      // Compute RSS link from post `id`
+      // This example assumes all posts are rendered as `/blog/[id]` routes
+      link: `/posts/${post.id}/`,
+    })),
     // (optional) inject custom xml
     customData: `<language>en-us</language>`,
   });
